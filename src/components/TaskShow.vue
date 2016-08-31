be1dde7c5badda17ae5483bbfef087d4b0c06e19
Shawn Tien <hightian@gmail.com>
2016-08-31 20:14:17 +0800

    add TaskShow.vue

---
+++ src/components/TaskShow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="task-show">
+    <div class="task">
+      <div class="header">
+        <h1>Task Details</h1>
+        <h2>Show details.</h2>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskShow',
+  components: {},
+  computed: {}
+}
+</script>
+
+
+<style>
+</style>
