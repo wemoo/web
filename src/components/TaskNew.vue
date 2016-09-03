6c2209a1f777c61947d94edd5485cc4771aac479
Shawn Tien <hightian@gmail.com>
2016-09-03 17:41:27 +0800

    update html

--- src/components/TaskNew.vue
+++ src/components/TaskNew.vue
@@ -1,40 +1,37 @@
 <template>
-  <div class="task-new">
-    <div class="task">
-      <div class="header">
-        <div class="page-title">New task</div>
-        <div class="page-desc">Create a new task.</div>
-      </div>
-      <div class="body">
-        <form class="pure-form pure-form-stacked">
-          <fieldset>
-            <div class="pure-control-group">
-                <label>Title</label>
-                <input id="title" v-model="taskTitle"
-                  type="text" class="pure-input-1" placeholder="Your task title">
-            </div>
-            <div class="pure-control-group">
-                <label for="description">Description</label>
-                <input id="description" v-model="taskDesc"
-                  type="text" class="pure-input-1" placeholder="Maybe a short description">
-            </div>
-            <div class="pure-control-group">
-                <label for="email">Script</label>
-                <textarea id="script" v-model="taskScript"
-                  class="pure-input-1" placeholder="Type your scipt here."></textarea>
-            </div>
-            
-          </fieldset>
-        </form>
-
-        <button @click="create()" class="pure-button pure-button-primary">Create</button>
-      </div>
+<div class="task-new">
+  <div class="task">
+    <div class="header">
+      <div class="page-title">New task</div>
+      <div class="page-desc">Create a new task.</div>
     </div>
+    <div class="body">
+      <form class="pure-form pure-form-stacked">
+        <fieldset>
+          <div class="pure-control-group">
+            <label>Title</label>
+            <input id="title" v-model="taskTitle" type="text" class="pure-input-1" placeholder="Your task title">
+          </div>
+          <div class="pure-control-group">
+            <label for="description">Description</label>
+            <input id="description" v-model="taskDesc" type="text" class="pure-input-1" placeholder="Maybe a short description">
+          </div>
+          <div class="pure-control-group">
+            <label for="email">Script</label>
+            <textarea id="script" v-model="taskScript" class="pure-input-1" placeholder="Type your scipt here."></textarea>
+          </div>
 
-    <pre>
+        </fieldset>
+      </form>
+
+      <button @click="create()" class="pure-button pure-button-primary">Create</button>
+    </div>
+  </div>
+
+  <pre>
       {{ task | json }}
     </pre>
-  </div>
+</div>
 </template>
 
 
